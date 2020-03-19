befa67c861b352e058d61525d41521486298892c
Gyalla <Gyallahorn@gmail.com>
2020-03-19 16:37:48 +0900

    test

--- app.js
+++ app.js
@@ -28,26 +28,6 @@ app.listen(port, () => console.log('Server running on port ' + port));
 
 
 
-
-
-
-app.get('/login', ensureToken, (req, res, next) => {
-
-    res.json({ msg: "success" })
-
-});
-function ensureToken(req, res, next) {
-    const bearerHeader = req.headers["authorization"];
-    if (typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(" ");
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
-        next();
-    } else {
-        res.sendStatus(403);
-    }
-}
-
 // api routes
 require('./models/user');
 app.use(require('./routes'));
--- controllers/users.js
+++ controllers/users.js
@@ -51,11 +51,7 @@ module.exports = {
         return res.status(200).json({ msg: "success", token: token });
     },
     signIn: async (req, res, next) => {
-        // const email = req.value.body.email; const password = req.value.body.password;
-        // User.findOne({ email }, (err, user) => {
-        //     const token = signToken(req.user);
-        //     res.status(200).json({ token });
-        // })
+
         jwt.verify(req.token, 'my_secret_key', (err, data) => {
             if (err) {
                 res.sendStatus(403);
@@ -78,67 +74,3 @@ module.exports = {
     }
 
 }
-
-
-// module.exports = {
-//     authenticate,
-//     getAll,
-//     getById,
-//     create,
-//     update,
-//     delete: _delete
-// };
-
-// async function authenticate({ username, password }) {
-//     const user = await User.findOne({ username });
-//     if (user && bcrypt.compareSync(password, user.hash)) {
-//         const { hash, ...userWithoutHash } = user.toObject();
-//         const token = jwt.sign({ sub: user.id }, config.secret);
-//         return {
-//             ...userWithoutHash,
-//             token
-//         };
-//     }
-// }
-
-// async function getAll() {
-//     return await User.find().select('-hash');
-// }
-
-// async function getById(id) {
-//     return await User.findById(id).select('-hash');
-// }
-
-// async function create(userParam) {
-//     // validate
-
-//     const email = userParam.email;
-//     const password = userParam.password;
-//     console.log(userParam.email + userParam.password)
-//     const newUser = new User({ email, password });
-//     await newUser.save
-
-// }
-// async function update(id, userParam) {
-//     const user = await User.findById(id);
-
-//     // validate
-//     if (!user) throw 'User not found';
-//     if (user.username !== userParam.username && await User.findOne({ username: userParam.username })) {
-//         throw 'Username "' + userParam.username + '" is already taken';
-//     }
-
-//     // hash password if it was entered
-//     if (userParam.password) {
-//         userParam.hash = bcrypt.hashSync(userParam.password, 10);
-//     }
-
-//     // copy userParam properties to user
-//     Object.assign(user, userParam);
-
-//     await user.save();
-// }
-
-// async function _delete(id) {
-//     await User.findByIdAndRemove(id);
-// }
